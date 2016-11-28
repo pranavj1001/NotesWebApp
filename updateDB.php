1b83cf0d6de3a548c95d854252e84ecf01eb0b11
pranavj1001 <pranavj1001@gmail.com>
2016-11-28 23:43:35 +0530

    updated the updateDB script

--- updateDB.php
+++ updateDB.php
@@ -1,7 +1,18 @@
 <?php
 
+    session_start();
+
     if(array_key_exists("content", $_POST)){
-        echo $_POST['content'];
+        
+        $link = mysqli_connect("localhost", "root", "", "noteswebapp");       
+        if (mysqli_connect_error()) {      
+            die ("Database Connection Error");         
+        }
+        
+        $query = "UPDATE `users` SET `notes` = '".mysqli_real_escape_string($link, $_POST['content'])."' WHERE id = ".mysqli_real_escape_string($link, $_SESSION['id'])." LIMIT 1";
+        
+        mysqli_query($link, $query);
+
     }
 
 ?>
