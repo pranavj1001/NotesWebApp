3b2e0c9d5a2c59a752067566a40d7ca748bd4495
pranavj1001 <pranavj1001@gmail.com>
2016-11-28 23:13:04 +0530

    some significant changes to loggedinpage

--- loggedinpage.php
+++ loggedinpage.php
@@ -60,8 +60,8 @@
       </ul>
     </nav>
       
-    <div class="container bg">
-      
+    <div class="container notesArea">
+          <textarea id="notes" class="form-control"></textarea>
     </div>
       
     <footer>
@@ -77,6 +77,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>    
   </body>
 </html>
